2fbb978829c59c22fab1f47aae902b20b2390d87
hyoyou <heatherhyou@gmail.com>
2017-10-24 21:53:38 -0400

    add category model, migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025015150) do
+ActiveRecord::Schema.define(version: 20171025015259) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
