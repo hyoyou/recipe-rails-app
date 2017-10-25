afd0e2c027989db15579947028cdef64c41b6d3a
hyoyou <heatherhyou@gmail.com>
2017-10-25 16:51:26 -0400

    set up associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+  has_many :ratings
 end
