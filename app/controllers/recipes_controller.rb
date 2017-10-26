96ae70c00fd9473be506df9a85be2364262457f5
hyoyou <heatherhyou@gmail.com>
2017-10-26 11:42:22 -0400

    add new and create actions for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,22 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :user_id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+recipe1 = Recipe.create(name: "Fettuccine Alfredo",
+                        description: "Bring a large pot of lightly salted water to a boil.
+                        Add fettuccini and cook for 8 to 10 minutes or until al dente; drain.
+                        In a large saucepan, melt butter into cream over low heat.
+                        Add salt, pepper and garlic salt.
+                        Stir in cheese over medium heat until melted; this will thicken the sauce.
+                        Add pasta to sauce.
+                        Use enough of the pasta so that all of the sauce is used and the pasta is thoroughly coated.
+                        Serve immediately.",
+                        user_id: 2)
+
+user = User.create(email: "heather@heatheryou.com", password: '12345678')
