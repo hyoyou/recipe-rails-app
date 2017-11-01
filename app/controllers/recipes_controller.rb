a2e7c90691562422526d32bbbda664c3f137f8ea
hyoyou <heatherhyou@gmail.com>
2017-11-01 15:20:38 -0400

    Add field for quantity in new recipe form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,13 +9,13 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
     7.times do
-      @recipe.ingredients.build
-      @recipe.recipe_ingredients.build
+      ingredient = @recipe.ingredients.build
+      ingredient.recipe_ingredients.build
     end
   end
 
   def create
-    #raise params.inspect
+    raise params.inspect
     @recipe = current_user.recipes.build(recipe_params)
     if @recipe.save
       redirect_to recipe_path(@recipe)
