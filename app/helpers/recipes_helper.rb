2a165b019b2f2f9c4f63ec91adb3fc1c11a8a083
hyoyou <heatherhyou@gmail.com>
2017-11-01 15:58:53 -0400

    Work on a helper method to save quantity to recipe

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,12 @@
 module RecipesHelper
+  def save_recipe_ingredients(recipe_params)
+    recipe_params[:ingredients_attributes].each do |ingredient_attribute|
+      if recipe_params[:ingredients_attributes][ingredient_attribute][:name] != ""
+        ingredient = Ingredient.find_or_create_by(name: recipe_params[:ingredients_attributes][ingredient_attribute][:name])
+        recipe_ingredient = RecipeIngredient.find_or_create_by(recipe_id: @recipe.id, ingredient_id: ingredient.id)
+        recipe_ingredient.quantity = recipe_params[:ingredients_attributes][ingredient_attribute][:recipe_ingredients_attributes]["0"][:quantity]
+        recipe_ingredient.save
+      end
+    end
+  end
 end
