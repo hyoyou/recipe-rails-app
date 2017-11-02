ecc4d6a40c6c7b17a2a576950e313a30ff33be7b
hyoyou <heatherhyou@gmail.com>
2017-11-02 13:28:43 -0400

    Add delete link for individual ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,4 +2,10 @@ class IngredientsController < ApplicationController
   def index
     @ingredients = Ingredient.all
   end
+
+  def destroy
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.destroy
+    redirect_to ingredients_path
+  end
 end
