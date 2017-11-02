d6dd2fe59081435d18bc55cc6817ab6d3d9c4c8c
hyoyou <heatherhyou@gmail.com>
2017-11-02 13:12:43 -0400

    Add links to recipes in index page to get to show page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  has_many :recipe_ingredients
+  has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients, dependent: :destroy
   has_many :ratings
 
