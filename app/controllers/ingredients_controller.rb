b4bb88c22760b1a7c39c14d294b2eb6bb0cb431b
hyoyou <heatherhyou@gmail.com>
2017-11-02 13:24:41 -0400

    Add index page for ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,5 @@
 class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+  end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,5 @@
 class Ingredient < ApplicationRecord
-  has_many :recipe_ingredients
+  has_many :recipe_ingredients, dependent: :destroy
   has_many :recipes, through: :recipe_ingredients, dependent: :destroy
 
   accepts_nested_attributes_for :recipe_ingredients, allow_destroy: true
