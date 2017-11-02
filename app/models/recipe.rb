2f72c3af4ed3fa5a58e9085d48ef898bf22272ed
hyoyou <heatherhyou@gmail.com>
2017-11-02 16:04:46 -0400

    Remove destroy dependency of ingredient to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,12 +2,12 @@ class Recipe < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :recipe_ingredients, dependent: :destroy
-  has_many :ingredients, through: :recipe_ingredients, dependent: :destroy
+  has_many :ingredients, through: :recipe_ingredients
   has_many :ratings
 
   validates :name, :description, presence: true
 
-  accepts_nested_attributes_for :ingredients, allow_destroy: true
+  accepts_nested_attributes_for :ingredients
   #accepts_nested_attributes_for :recipe_ingredients
 
   #"ingredients_attributes"=>{"0"=>{"name"=>"Fettucine"}, "1"=>{"name"=>"Heavy Cream"}, "2"=>{"name"=>"Parmesan Cheese"}}
