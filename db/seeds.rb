0d3c5641c43626418533d2e1905164a9923a0a45
hyoyou <heatherhyou@gmail.com>
2017-11-03 17:40:50 -0400

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,38 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-recipe1 = Recipe.create(name: "Fettuccine Alfredo",
-                        description: "Bring a large pot of lightly salted water to a boil.
-                        Add fettuccini and cook for 8 to 10 minutes or until al dente; drain.
-                        In a large saucepan, melt butter into cream over low heat.
-                        Add salt, pepper and garlic salt.
-                        Stir in cheese over medium heat until melted; this will thicken the sauce.
-                        Add pasta to sauce.
-                        Use enough of the pasta so that all of the sauce is used and the pasta is thoroughly coated.
-                        Serve immediately.",
-                        user_id: 2,
-                        category_id: 2)
+Category.create(name: "Appetizer")
+Category.create(name: "Main Dish")
+Category.create(name: "Side Dish")
+Category.create(name: "Soup")
+Category.create(name: "Salad")
+Category.create(name: "Dessert")
+Category.create(name: "Beverage")
 
-ingredient1 = Ingredient.create(name: "Fettucine")
-ingredient2 = Ingredient.create(name: "Butter")
-ingredient3 = Ingredient.create(name: "Heavy Cream")
-ingredient4 = Ingredient.create(name: "Salt")
-ingredient5 = Ingredient.create(name: "Pepper")
-ingredient6 = Ingredient.create(name: "Parmesan Cheese")
+Recipe.create(name: "Fettuccine Alfredo",
+              description: "Bring a large pot of lightly salted water to a boil.
+              Add fettuccini and cook for 8 to 10 minutes or until al dente; drain.
+              In a large saucepan, melt butter into cream over low heat.
+              Add salt, pepper and garlic salt.
+              Stir in cheese over medium heat until melted; this will thicken the sauce.
+              Add pasta to sauce.
+              Use enough of the pasta so that all of the sauce is used and the pasta is thoroughly coated.
+              Serve immediately.",
+              user_id: 1,
+              category_id: 2)
 
-recipe_ingredient1 = RecipeIngredient.create(recipe_id: 18, ingredient_id: 22, quantity: "1 Box", key_ingredient: true)
-recipe_ingredient1 = RecipeIngredient.create(recipe_id: 18, ingredient_id: 23, quantity: "1 Cup", key_ingredient: false)
-recipe_ingredient1 = RecipeIngredient.create(recipe_id: 18, ingredient_id: 24, quantity: "3/4 Pint", key_ingredient: false)
-recipe_ingredient1 = RecipeIngredient.create(recipe_id: 18, ingredient_id: 25, quantity: "1 tsp", key_ingredient: false)
-recipe_ingredient1 = RecipeIngredient.create(recipe_id: 18, ingredient_id: 26, quantity: "1 tsp", key_ingredient: false)
-recipe_ingredient1 = RecipeIngredient.create(recipe_id: 18, ingredient_id: 27, quantity: "1/2 Cup", key_ingredient: false)
+Ingredient.create(name: "Fettucine")
+Ingredient.create(name: "Butter")
+Ingredient.create(name: "Heavy Cream")
+Ingredient.create(name: "Salt")
+Ingredient.create(name: "Pepper")
+Ingredient.create(name: "Parmesan Cheese")
 
-user = User.create(email: "heather@heatheryou.com", password: '12345678')
+RecipeIngredient.create(recipe_id: 1, ingredient_id: 1, quantity: "1 Box", key_ingredient: true)
+RecipeIngredient.create(recipe_id: 1, ingredient_id: 2, quantity: "1 Cup", key_ingredient: false)
+RecipeIngredient.create(recipe_id: 1, ingredient_id: 3, quantity: "3/4 Pint", key_ingredient: false)
+RecipeIngredient.create(recipe_id: 1, ingredient_id: 4, quantity: "1 tsp", key_ingredient: false)
+RecipeIngredient.create(recipe_id: 1, ingredient_id: 5, quantity: "1 tsp", key_ingredient: false)
+RecipeIngredient.create(recipe_id: 1, ingredient_id: 6, quantity: "1/2 Cup", key_ingredient: false)
+
+User.create(email: "heather@heatheryou.com", password: '12345678')
