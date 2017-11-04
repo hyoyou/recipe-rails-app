fd5c3de68fa07d7e082958ae39f4ee057ada9d5e
hyoyou <heatherhyou@gmail.com>
2017-11-04 12:30:29 -0400

    Remove helper method since it will not be used in views. Moved into controller for now

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class RecipesController < ApplicationController
-  include RecipesHelper
+  #include RecipesHelper
 
   def index
     @recipes = Recipe.all
@@ -64,4 +64,20 @@ class RecipesController < ApplicationController
                                    ingredients_ids: [],
                                    ingredients_attributes: [:name, recipe_ingredients_attributes: [:quantity, :key_ingredient]])
   end
+
+  def save_recipe_ingredients(recipe_params)
+    if recipe_params[:ingredients_attributes]
+      recipe_params[:ingredients_attributes].each do |ingredient_attribute|
+        if !recipe_params[:ingredients_attributes][ingredient_attribute][:name].blank? && !recipe_params[:ingredients_attributes][ingredient_attribute][:recipe_ingredients_attributes]["0"][:quantity].blank?
+          ingredient_name = recipe_params[:ingredients_attributes][ingredient_attribute][:name].capitalize
+          ingredient = Ingredient.find_or_create_by(name: ingredient_name)
+          recipe_ingredient = RecipeIngredient.find_or_create_by(recipe_id: @recipe.id, ingredient_id: ingredient.id)
+          recipe_ingredient.quantity = recipe_params[:ingredients_attributes][ingredient_attribute][:recipe_ingredients_attributes]["0"][:quantity]
+          recipe_ingredient.key_ingredient = recipe_params[:ingredients_attributes][ingredient_attribute][:recipe_ingredients_attributes]["0"][:key_ingredient]
+          recipe_ingredient.save
+        end
+      end
+    end
+  end
+  
 end
--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,16 +1,3 @@
 module RecipesHelper
-  def save_recipe_ingredients(recipe_params)
-    if recipe_params[:ingredients_attributes]
-      recipe_params[:ingredients_attributes].each do |ingredient_attribute|
-        if !recipe_params[:ingredients_attributes][ingredient_attribute][:name].blank? && !recipe_params[:ingredients_attributes][ingredient_attribute][:recipe_ingredients_attributes]["0"][:quantity].blank?
-          ingredient_name = recipe_params[:ingredients_attributes][ingredient_attribute][:name].capitalize
-          ingredient = Ingredient.find_or_create_by(name: ingredient_name)
-          recipe_ingredient = RecipeIngredient.find_or_create_by(recipe_id: @recipe.id, ingredient_id: ingredient.id)
-          recipe_ingredient.quantity = recipe_params[:ingredients_attributes][ingredient_attribute][:recipe_ingredients_attributes]["0"][:quantity]
-          recipe_ingredient.key_ingredient = recipe_params[:ingredients_attributes][ingredient_attribute][:recipe_ingredients_attributes]["0"][:key_ingredient]
-          recipe_ingredient.save
-        end
-      end
-    end
-  end
+
 end
