99debd161e8a72abc9ce1e12c6397b7bb0b9e601
hyoyou <heatherhyou@gmail.com>
2017-11-07 15:14:05 -0500

    Add link to add rating on recipe show page

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,3 +1,24 @@
 class RatingsController < ApplicationController
+  def index
+    @ratings =  Rating.all
+  end
 
+  def new
+    raise params.inspect
+    @rating = Rating.new
+  end
+
+  def create
+    @rating = Rating.new(rating_params)
+    if @rating.save?
+      redirect_to recipe_ratings_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def rating_params
+    params.require(:rating).permit(:rating, :user_id, :recipe_id)
+  end
 end
