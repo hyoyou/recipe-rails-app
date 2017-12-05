a492c665a2481847a0391db073f771efa9349bf4
hyoyou <heatherhyou@gmail.com>
2017-12-05 15:50:56 -0500

    Add validation to ingredient to only allow ingredient names beginning with alphabet

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,6 @@ class Ingredient < ApplicationRecord
   has_many :recipe_ingredients, dependent: :destroy
   has_many :recipes, through: :recipe_ingredients
 
-  validates :name, uniqueness: true, presence: true
+  validates :name, uniqueness: true, presence: true, format: { with: /[a-zA-Z]/ }
 
 end
