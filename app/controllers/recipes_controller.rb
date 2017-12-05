58c3e776ea4f7ff3ce4ffc9d7d66f791bd0eb0f9
hyoyou <heatherhyou@gmail.com>
2017-12-05 11:36:27 -0500

    Update strong params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -33,7 +33,6 @@ class RecipesController < ApplicationController
   def create
     @recipe = current_user.recipes.build(recipe_params)
     if @recipe.save
-      save_recipe_ingredients(recipe_params)
       redirect_to recipe_path(@recipe)
     else
       render :new
@@ -41,9 +40,9 @@ class RecipesController < ApplicationController
   end
 
   def update
+    #binding.pry
     @recipe.update(recipe_params)
     if @recipe.save
-      save_recipe_ingredients(recipe_params)
       redirect_to recipe_path(@recipe)
     else
       render :edit
@@ -65,27 +64,8 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:name,
-                                   :description,
-                                   :user_id,
-                                   :category_id,
-                                   ingredients_ids: [],
-                                   ingredients_attributes: [:name, recipe_ingredients_attributes: [:quantity, :key_ingredient]])
-  end
-
-  def save_recipe_ingredients(recipe_params)
-    if recipe_params[:ingredients_attributes]
-      recipe_params[:ingredients_attributes].each do |ingredient_attribute|
-        if !recipe_params[:ingredients_attributes][ingredient_attribute][:name].blank? && !recipe_params[:ingredients_attributes][ingredient_attribute][:recipe_ingredients_attributes]["0"][:quantity].blank?
-          ingredient_name = recipe_params[:ingredients_attributes][ingredient_attribute][:name].capitalize
-          ingredient = Ingredient.find_or_create_by(name: ingredient_name)
-          recipe_ingredient = RecipeIngredient.find_or_create_by(recipe_id: @recipe.id, ingredient_id: ingredient.id)
-          recipe_ingredient.quantity = recipe_params[:ingredients_attributes][ingredient_attribute][:recipe_ingredients_attributes]["0"][:quantity]
-          recipe_ingredient.key_ingredient = recipe_params[:ingredients_attributes][ingredient_attribute][:recipe_ingredients_attributes]["0"][:key_ingredient]
-          recipe_ingredient.save
-        end
-      end
-    end
+    params.require(:recipe).permit(:name, :description, :user_id, :category_id, ingredient_ids: [],
+                                   recipe_ingredients_attributes: [:id, :quantity, :key_ingredient, ingredient_attributes: [:id, :name]])
   end
 
 end
