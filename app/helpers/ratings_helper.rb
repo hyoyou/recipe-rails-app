bb20b1f9789bb79bdc2891e10831ac512a352ee1
hyoyou <heatherhyou@gmail.com>
2017-12-10 20:51:30 -0500

    Move find_key_ingredients helper method into recipes helper

--- app/helpers/ratings_helper.rb
+++ app/helpers/ratings_helper.rb
@@ -1,4 +1,5 @@
 module RatingsHelper
+  
   def ratings_average(recipe)
     ratings_array = []
     recipe.ratings.each do |rating|
@@ -20,13 +21,4 @@ module RatingsHelper
     r.rating
   end
 
-  def find_key_ingredients(recipe)
-    key_ingredient_array = RecipeIngredient.where(recipe_id: recipe.id, key_ingredient: true).to_a
-    html = ''
-    key_ingredient_array.map do |k|
-      ingredient = Ingredient.find(k.ingredient_id)
-      html += link_to(ingredient.name, ingredient_recipes_path(ingredient), :recipe_id => recipe.id)
-    end
-    html.html_safe
-  end
 end
--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,3 +1,11 @@
 module RecipesHelper
-
+  def find_key_ingredients(recipe)
+    key_ingredient_array = RecipeIngredient.where(recipe_id: recipe.id, key_ingredient: true).to_a
+    html = ''
+    key_ingredient_array.map do |k|
+      ingredient = Ingredient.find(k.ingredient_id)
+      html += link_to(ingredient.name, ingredient_recipes_path(ingredient), :recipe_id => recipe.id)
+    end
+    html.html_safe
+  end
 end
