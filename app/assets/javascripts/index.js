66e168354d5babad3530f0b5fb6c9cbca1ef01be
Heather You <heatherhyou@gmail.com>
2018-02-21 22:59:06 -0500

    build recipe object constructor and formatRecipe prototype

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -7,7 +7,29 @@ function attachEventListeners() {
     e.preventDefault();
 
     $.get('/recipes.json', function(data) {
-      console.log(data);
+      //console.log(data);
+      data.forEach(function(recipe) {
+        //console.log(recipe.id)
+        var newRecipe = new Recipe(recipe.id, recipe.name, recipe.description);
+        var formattedRecipe = newRecipe.formatRecipe();
+        $('.recipe-container').html('');
+        console.log(formattedRecipe)
+        //$('.recipe-container').append(formattedRecipe);
+      })
     })
   })
+}
+
+function Recipe(id, name, description) {
+  this.id = id;
+  this.name = name;
+  this.description = description
+}
+
+Recipe.prototype.formatRecipe = function() {
+  let recipeHtml = ''
+  recipeHtml += '<h1>' + this.name + '</h1>';
+  recipeHtml += '<p>' + this.description + '</p>';
+
+  return recipeHtml;
 }
