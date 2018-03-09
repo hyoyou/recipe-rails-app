41b2dd7dde2fa8596c098dc1d29eb61550332079
Heather You <heatherhyou@gmail.com>
2018-03-09 10:52:52 -0500

    create comments index to load via rails, no ajax yet

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,29 @@
 class CommentsController < ApplicationController
+  before_action :set_recipe
+
+  def index
+    @comments = @recipe.comments
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = @recipe.comments.build(comments_params)
+    if @comment.save
+      redirect_to recipe_path
+    else
+      redirect_to new_recipe_comment_path(@recipe)
+    end
+  end
+
+  private
+  def set_recipe
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+
+  def comments_params
+    params.require(:comment).permit(:body)
+  end
 end
